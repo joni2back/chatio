b07e26448375e8a729d6c4c0d445985f59d5420b
joni2back <joni2back@gmail.com>
2019-08-11 22:47:12 -0300

    lazy load for history

--- routes/routes.js
+++ routes/routes.js
@@ -3,24 +3,36 @@ const router = express.Router();
 const ChatMessage  = require('../mongoose-models/ChatMessage');
 const SessionChecker = require('../services/SessionChecker');
 
-router.get('/history/:roomId/:sessionId', (req, res) => {
+router.get('/history/:roomId/:sessionId/:lastOne?', (req, res) => {
 
     const roomId = req.params.roomId;
     const sessionId = req.params.sessionId;
-    const limit = 100;
+    const lastOne = req.params.lastOne;
+    const limit = 10;
+    const sort = {
+        date: -1 
+    };
+
+    const cond = {
+        roomId: roomId,
+    };
+
+    if (/[a-f\d]{24}/.test(lastOne)) {
+        cond._id = {
+            $lt: lastOne
+        }
+    }
 
     if (! SessionChecker(sessionId)) {
         res.status(404).send('Not found');
     }
 
     //implement pagination for lazy load
-    var result = ChatMessage.find({
-        roomId: roomId
-    }).sort({
-        date: -1
-    }).limit(limit).then(r => {
+    var result = ChatMessage.find(cond, {}, {sort}).limit(limit).then(r => {
         res.send(r.reverse());
-    });
+    }).catch(e => {
+        res.status(500).send(e);
+    })
 });
 
 module.exports = router;
